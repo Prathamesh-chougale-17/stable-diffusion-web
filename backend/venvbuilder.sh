9e8a9931c29d6edd723fd1dad97211a8747de96b
Prathamesh Chougale <prathamesh17170@gmail.com>
2025-01-29 20:43:04 +0530

    initialize frontend and backend structure with Next.js, ESLint, Tailwind CSS, and virtual environment setup

---
+++ backend/venvbuilder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create a virtual environment
+python -m venv venv
+
+# Activate the virtual environment
+source venv/Scrips/activate
+
+# Install the dependencies
+pip install -r requirements.txt
